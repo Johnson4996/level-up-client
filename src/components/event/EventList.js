ff24b92f86af439dbf0df47d02a6e8baa3029e53
Johnson4996 <Johnson.4996@gmail.com>
2020-11-06 11:22:03 -0600

    users now able to join events

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -3,7 +3,7 @@ import { EventContext } from "./EventProvider.js"
 import "./event.css"
 
 export const EventList = (props) => {
-    const { events, getEvents } = useContext(EventContext)
+    const { events, getEvents,joinEvent } = useContext(EventContext)
 
     useEffect(() => {
         getEvents()
@@ -21,24 +21,20 @@ export const EventList = (props) => {
                 <h1>Level Up Game Events</h1>
             </header>
             {
-                events.map(event => {
-                    return <section key={event.id} className="registration">
-                        <div className="registration__game">{event.game.title}</div>
-                        <div>{event.description}</div>
-                        <div>
-                            {
-                                new Date().toLocaleDateString("en-US",
-                                {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric'
-                                })
-                            } @ {event.time}
-                            
-                        </div>
-                    </section>
-                })
+                
+                    events.map(event => {
+                        return <section key={event.id} className="registration">
+                            <div className="registration__game">{event.game.title}</div>
+                            <div>{event.description}</div>
+                            <div>
+                                {event.date} @ {event.time}
+                            </div>
+                            <button className="btn btn-2"
+                                    onClick={() => joinEvent(event.id)}
+                            >Join</button>
+                        </section>
+                    })
+                
             }
         </article >
         </>
